abd2813148a85e9f58e841970412c918a8c197c7
Sean Boots <sean.boots@gmail.com>
2026-01-20 17:13:51 -0700

    Initial commit with retrieval code and templates

---
+++ retrieve.R
@@ -0,0 +1,119 @@
+library(tidyverse)
+library(fs)
+library(readxl)
+library(rmarkdown)
+library(janitor)
+library(rvest)
+
+run_start_time <- now()
+paste("Start time:", run_start_time)
+
+news_releases <- read_xlsx("input/yukon.ca-news-releases-published-2018-2021.xlsx")
+
+html_template_start <- read_file("templates/start.html")
+html_template_end <- read_file("templates/end.html")
+
+
+# Testing: limit to a subset of news releases
+news_releases <- news_releases |> 
+  slice_head(n = 10)
+
+# Generate the current year from the date published field
+news_releases <- news_releases |> 
+  mutate(
+    year = str_sub(publish_date, 0L, 4L)
+  )
+
+retrieve_individual_news_release <- function(page_url, year, news_release_number, language, title, description) {
+  
+  html_output_path <- path("output", language, year, str_c(news_release_number, ".html"))
+  
+  if(file_exists(html_output_path)) {
+    cat(str_c("- Path ", html_output_path, " already exists.\n"))
+    return()
+  }
+  
+  news_release_html <- read_html(page_url) |> 
+    html_element("main")
+  
+  remove_node_feedback_form <- news_release_html |> 
+    html_node("div#block-page-feedback-webform")
+  
+  remove_node_date_modified <- news_release_html |> 
+    html_node("div.node-date-modified")
+  
+  remove_node_breadcrumbs <- news_release_html |> 
+    html_node("div.region-breadcrumb")
+  
+
+  
+  # Thanks to
+  # https://stackoverflow.com/a/50769954/756641
+  xml2::xml_remove(remove_node_feedback_form)
+  xml2::xml_remove(remove_node_date_modified)
+  xml2::xml_remove(remove_node_breadcrumbs)
+  
+  dir_create(path("output", language, year))
+  
+  # paste(path("output", language, year, str_c(news_release_number, ".html")))
+  
+  # paste(news_release_html)
+  
+  formatted_html_template_start <- html_template_start |> 
+    str_replace("%%TITLE%%", title) |> 
+    str_replace("%%DESCRIPTION%%", description)
+    
+  # Update image paths (still loading from Yukon.ca for now)
+  # Update Yukon.ca links too
+  formatted_news_release_html <- as.character(news_release_html) |> 
+    str_replace_all('src="/sites/default/', 'src="https://yukon.ca/sites/default/') |> 
+    str_replace_all('href="/en/', 'href="https://yukon.ca/en/') |> 
+    str_replace_all('href="/fr/', 'href="https://yukon.ca/fr/')
+  
+  
+  news_release_output <- str_c(
+    formatted_html_template_start,
+    formatted_news_release_html,
+    html_template_end
+  )
+  
+  write_file(
+    news_release_output,
+    file = html_output_path
+    )
+  
+}
+
+
+# retrieve_individual_news_release(
+#   "https://yukon.ca/en/news/premier-sandy-silver-congratulates-yukons-2018-olympic-athletes", 
+#   "2018", 
+#   "18-024", 
+#   "en", 
+#   "Congrats!", 
+#   "Meta description here."
+#   )
+
+for (i in seq_along(news_releases$node_id)) { 
+  cat("Retrieving", news_releases$page_url[i], "\n")
+  
+  retrieve_individual_news_release(
+    news_releases$page_url[i],
+    news_releases$year[i],
+    news_releases$news_release_number[i],
+    news_releases$language[i],
+    news_releases$title[i],
+    news_releases$meta_description[i]
+  )
+  
+  # Be gentle to the server between requests!
+  Sys.sleep(0.5)
+  
+}
+
+
+run_end_time <- now()
+paste("Start time was:", run_start_time)
+paste("End time was:", run_end_time)
+
+paste("Elapsed time was", round(time_length(interval(run_start_time, run_end_time), "hours"), digits = 2), "hours")
